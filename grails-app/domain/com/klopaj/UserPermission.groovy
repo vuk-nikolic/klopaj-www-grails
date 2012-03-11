988bcb30aebcc20a200a126d9d59655660174f6b
Vuk Nikolic <vuk@gobandit.com>
2012-03-11 17:06:25 +0100

    - Another rj* field renamed

--- grails-app/domain/com/klopaj/UserPermission.groovy
+++ grails-app/domain/com/klopaj/UserPermission.groovy
@@ -11,20 +11,20 @@ class UserPermission {
          table 'rj_permission'
          // version is set to false, because this isn't available by default for legacy databases
          version false
-         rjRoleList column:'PERMISSION_ID',joinTable:'rj_role_permission'
+         roles column:'PERMISSION_ID',joinTable:'rj_role_permission'
          id generator:'identity', column:'PERMISSION_ID'
     }
     Integer version
     String name
 
-    static hasMany = [ rjRoleList : Role ]
+    static hasMany = [ roles: Role ]
 
     static belongsTo = [  Role ]
 
     static constraints = {
         version(max: 2147483647)
         name(size: 1..20, blank: false)
-        rjRoleList()
+        roles()
     }
     String toString() {
         return "${id}"
