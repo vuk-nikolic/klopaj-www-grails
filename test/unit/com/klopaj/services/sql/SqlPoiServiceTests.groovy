dd6593ce12ae1ebe049ffe7c7cd8ac836ab34eb0
Vuk Nikolic <vuk@gobandit.com>
2012-08-18 01:07:03 +0200

    - Draft test for addPoi

--- test/unit/com/klopaj/services/sql/SqlPoiServiceTests.groovy
+++ test/unit/com/klopaj/services/sql/SqlPoiServiceTests.groovy
@@ -1,14 +1,83 @@
+/*
+ * Copyright (c) 2012. klopaj.com
+ */
+
 package com.klopaj.services.sql
 
 import grails.test.mixin.TestFor
+import com.klopaj.Poi
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(SqlPoiService)
 class SqlPoiServiceTests {
+    def poiService
 
-    void testSomething() {
-        fail "Implement me"
+    void testAddPoi() {
+        int count = Poi.findAll().size();
+
+        Poi newPoi = new Poi()
+        newPoi.name = "Test"
+        newPoi.address = "Address"
+        newPoi.contactEmail = "email@klopaj.com"
+        newPoi.contactFax = "fax number"
+        newPoi.contactTel1 = "33333"
+        newPoi.contactTel2 = "44444"
+        newPoi.contactWww = "www.klopaj.com"
+
+        // Check if saving passed
+        assert poiService.addPoi(newPoi)
+
+        // Check if number of pois increased
+        int newCount = Poi.findAll().size()
+        assert newCount == count
+
+        // Check if we load it from db
+        Poi fromDb = Poi.findByName(newPoi.name)
+        assert fromDb.address == newPoi.address
+
+        // Clean up
+        fromDb.delete()
+    }
+
+    void testUpdatePoi() {
+
+    }
+
+    void testVoteUp() {
+
+    }
+
+    void testVoteDown() {
+
+    }
+
+    void testGetVotes() {
+
+    }
+
+    void testFollow() {
+
+    }
+
+    void testUnfollow() {
+
+    }
+
+    void testGetComments() {
+
+    }
+
+    void testGetFavorites() {
+
+    }
+
+    void testAddComment() {
+
+    }
+
+    void testGetPhotos() {
+
     }
 }
